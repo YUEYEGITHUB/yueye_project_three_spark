5d7b211f502643a21dbb5cc7b441d66c2630a545
root <root@xuqingshen-1.(none)>
2015-04-30 05:37:49 -0500

    fix the script content xuqingshen change  DX3

--- installation-tools/master/config-system.sh
+++ installation-tools/master/config-system.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 mv /etc/hadoop/conf/core-site.xml /etc/hadoop/conf/core-site.xml.bak
-wget http://DX2/xuqingshen-conf/core-site.xml -O /etc/hadoop/conf/core-site.xml
+wget http://DX2/DX3-conf/core-site.xml -O /etc/hadoop/conf/core-site.xml
 mv /etc/hadoop/conf/hdfs-site.xml /etc/hadoop/conf/hdfs-site.xml.bak
-wget http://DX2/xuqingshen-conf/hdfs-site.xml -O /etc/hadoop/conf/hdfs-site.xml
+wget http://DX2/DX3-conf/hdfs-site.xml -O /etc/hadoop/conf/hdfs-site.xml
 mv /etc/hadoop/conf/yarn-site.xml /etc/hadoop/conf/yarn-site.xml.bak
-wget http://DX2/xuqingshen-conf/yarn-site.xml -O /etc/hadoop/conf/yarn-site.xml
+wget http://DX2/DX3-conf/yarn-site.xml -O /etc/hadoop/conf/yarn-site.xml
 mv /etc/hadoop/conf/mapred-site.xml /etc/hadoop/conf/mapred-site.xml.bak
-wget http://DX2/xuqingshen-conf/mapred-site.xml -O /etc/hadoop/conf/mapred-site.xml
+wget http://DX2/DX3-conf/mapred-site.xml -O /etc/hadoop/conf/mapred-site.xml
 mv /etc/hbase/conf/hbase-site.xml /etc/hbase/conf/hbase-site.xml.bak
-wget http://DX2/xuqingshen-conf/hbase-site.xml -O /etc/hbase/conf/hbase-site.xml
+wget http://DX2/DX3-conf/hbase-site.xml -O /etc/hbase/conf/hbase-site.xml
 mv /etc/zookeeper/conf/zoo.cfg /etc/zookeeper/conf/zoo.cfg.bak
-wget http://DX2/xuqingshen-conf/zoo.cfg -O /etc/zookeeper/conf/zoo.cfg
+wget http://DX2/DX3-conf/zoo.cfg -O /etc/zookeeper/conf/zoo.cfg
 
 for host in `cat h.slaves`; do
   rsync /etc/hadoop/conf/core-site.xml $host:/etc/hadoop/conf/core-site.xml
